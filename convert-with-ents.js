5a91a225359c6995dc7a45d014087fa0881152cb
Rairye <37942271+Rairye@users.noreply.github.com>
2021-10-27 12:23:13 +0900

    Add files via upload

---
+++ convert-with-ents.js
@@ -0,0 +1,44 @@
+/*
+Copyright 2021 Rairye
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+var charMap = new Map([["&", "&amp;"], ["<", "&lt;"], [">", "&gt;"], ["\"", "&quot;"], ["\'", "&#39;"]]);
+
+function convertWithEnts(inputStr) {
+	if (typeof inputStr != "string") {
+		return inputStr;
+	}
+	
+	var result = "";
+	var lastReplacement = 0;
+	var stringLength = inputStr.length;
+	
+	for (var i = 0; i < stringLength; i++) {
+		var currentChar = inputStr.charAt(i);
+		if (charMap.has(currentChar)) {
+		   var replacement = charMap.get(currentChar);
+		   result+= (i > lastReplacement ? (inputStr.substring(lastReplacement, i) + replacement) : replacement);
+		   lastReplacement = i + 1;
+		}
+
+	}
+	
+	if (lastReplacement == 0) {
+		return inputStr;
+	}
+	
+	return (lastReplacement == stringLength ? result : result + inputStr.substring(lastReplacement));
+	
+}
